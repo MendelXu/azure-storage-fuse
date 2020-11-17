6d89bab2d1cd7c007834bb9741cd663f4d816dbc
nara <narven@microsoft.com>
2020-11-17 13:02:46 -0800

    merged latest from maste

--- blobfuse/main.cpp
+++ blobfuse/main.cpp
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
     {
         return ret;
     }
-    // commenting out the below as it is to take care of non-thread safe for gnutls before 3.3
-    //ret = configure_tls();
+
+    ret = configure_tls();
     if (ret != 0)
     {
         return ret;
@@ -29,8 +29,7 @@ int main(int argc, char *argv[])
 
     ret =  fuse_main(args.argc, args.argv, &azs_blob_operations, NULL);
 
-//see gnutls comment above
-   // gnutls_global_deinit();
+    gnutls_global_deinit();
 
     return ret;
 }
